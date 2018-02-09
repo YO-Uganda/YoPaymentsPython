a1c7760b509eabfdca7967033a95fefcde6f54d7
Aziz Kirumira <akirumira@yo.co.ug>
2018-02-09 16:58:33 +0300

    redefining the test file before submit

--- YoPaymentsAPITest.py
+++ YoPaymentsAPITest.py
@@ -1,13 +1,11 @@
 import yopayments
 
-YoPay = yopayments.YoPay("100017341111", "zxfV-COuR-lZJs-li2v-xreU-gNDS-A7Jk-D0Ea")
-# YoPay.set_instant_payment_notification_url("http://165.227.133.30/yo/YoPaymentsIPN_PHP/index.php")
-# YoPay.set_external_reference("AzizPythonTest2")
-# response = YoPay.ac_deposit_funds("256779664901", 500, "Testing the python library 1")
-response = YoPay.ac_acct_balance()
-print(response)
+YoPay = yopayments.YoPay("XXXXXX", "XXXXXXXX")
+YoPay.set_instant_payment_notification_url("http://XXXXXXXXXXXX.com/yo/receiveIPN")
+YoPay.set_external_reference("YoPaymentsAPIPythonTest")
+response = YoPay.ac_deposit_funds("256700000000", 500, "Testing the python library")
 
 if response.get("Status") == "OK":
     print(response.get("Status"))
 else:
-    print("Python sucks..." + response.get("Status"))
+    print("There was an error processing the payment..." + response.get("StatusCode"))
