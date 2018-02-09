34837fe2cbfcdc5e74c8f0edf16bcdece08e4a71
Isaac Obella <wizlif@users.noreply.github.com>
2018-02-09 12:34:35 +0300

    Updated Yo Api to use xmltodict for xml parsing
    xml.dom.minidom throwing errors

--- yopayments.py
+++ yopayments.py
@@ -1,5 +1,5 @@
 import requests
-from xml.dom.minidom import parseString
+import xmltodict
 
 
 class YoPay:
@@ -191,53 +191,8 @@ class YoPay:
         xml += "</AutoCreate>"
 
         response = self.__get_xml_response(xml)
-        result = parseString(response)
 
-        status = self.__get_text(result.getElementsByTagName("Status")[0].childNodes)
-        status_code = self.__get_text(result.getElementsByTagName("StatusCode")[0].childNodes)
-
-        status_message = None
-        if len(result.getElementsByTagName("StatusMessage")) > 0:
-            status_message = self.__get_text(result.getElementsByTagName("StatusMessage")[0].childNodes)
-
-        transaction_status = None
-        if len(result.getElementsByTagName("TransactionStatus")) > 0:
-            transaction_status = self.__get_text(result.getElementsByTagName("TransactionStatus")[0].childNodes)
-
-        error_message_code = None
-        if len(result.getElementsByTagName("ErrorMessageCode")) > 0:
-            error_message_code = self.__get_text(result.getElementsByTagName("ErrorMessageCode")[0].childNodes)
-
-        error_message = None
-        if len(result.getElementsByTagName("ErrorMessage")) > 0:
-            error_message = self.__get_text(result.getElementsByTagName("ErrorMessage")[0].childNodes)
-
-        transaction_reference = None
-        if len(result.getElementsByTagName("TransactionReference")) > 0:
-            transaction_reference = self.__get_text(result.getElementsByTagName("TransactionReference")[0].childNodes)
-
-        mnotransaction_reference_id = None
-        if len(result.getElementsByTagName("MNOTransactionReferenceId")) > 0:
-            mnotransaction_reference_id = self.__get_text(
-                result.getElementsByTagName("MNOTransactionReferenceId")[0].childNodes)
-
-        issued_receipt_number = None
-        if len(result.getElementsByTagName("IssuedReceiptNumber")) > 0:
-            issued_receipt_number = self.__get_text(result.getElementsByTagName("IssuedReceiptNumber")[0].childNodes)
-
-        response_object = {
-            "Status": status,
-            "StatusCode": status_code,
-            "StatusMessage": status_message,
-            "ErrorMessage": error_message,
-            "ErrorMessageCode": error_message_code,
-            "TransactionReference": transaction_reference,
-            "TransactionStatus": transaction_status,
-            "MNOTransactionReferenceId": mnotransaction_reference_id,
-            "IssuedReceiptNumber": issued_receipt_number
-        }
-
-        return response_object
+        return xmltodict.parse(response)["AutoCreate"]["Response"]
 
     def ac_transaction_check_status(self, transaction_reference, private_transaction=None):
         """
@@ -268,66 +223,8 @@ class YoPay:
         xml += "</AutoCreate>"
 
         response = self.__get_xml_response(xml)
-        result = parseString(response)
 
-        error_message_code = None
-        if len(result.getElementsByTagName("ErrorMessageCode")) > 0:
-            error_message_code = self.__get_text(
-                result.getElementsByTagName("ErrorMessageCode")[0].childNodes)
-
-        error_message = None
-        if len(result.getElementsByTagName("ErrorMessage")) > 0:
-            error_message = self.__get_text(result.getElementsByTagName("ErrorMessage")[0].childNodes)
-
-        transaction_reference = None
-        if len(result.getElementsByTagName("TransactionReference")) > 0:
-            transaction_reference = self.__get_text(
-                result.getElementsByTagName("TransactionReference")[0].childNodes)
-
-        mnotransaction_reference_id = None
-        if len(result.getElementsByTagName("MNOTransactionReferenceId")) > 0:
-            mnotransaction_reference_id = self.__get_text(
-                result.getElementsByTagName("MNOTransactionReferenceId")[0].childNodes)
-
-        amount = None
-        if len(result.getElementsByTagName("Amount")) > 0:
-            amount = self.__get_text(result.getElementsByTagName("Amount")[0].childNodes)
-
-        amount_formatted = None
-        if len(result.getElementsByTagName("AmountFormatted")) > 0:
-            amount_formatted = self.__get_text(result.getElementsByTagName("AmountFormatted")[0].childNodes)
-
-        currency_code = None
-        if len(result.getElementsByTagName("CurrencyCode")) > 0:
-            currency_code = self.__get_text(result.getElementsByTagName("CurrencyCode")[0].childNodes)
-
-        transaction_init_date = None
-        if len(result.getElementsByTagName("TransactionInitialDate")) > 0:
-            transaction_init_date = self.__get_text(result.getElementsByTagName("TransactionInitialDate")[0].childNodes)
-
-        transaction_comp_date = None
-        if len(result.getElementsByTagName("TransactionCompletionDate")) > 0:
-            transaction_comp_date = self.__get_text(
-                result.getElementsByTagName("TransactionCompletionDate")[0].childNodes)
-
-        issued_receipt_number = None
-        if len(result.getElementsByTagName("IssuedReceiptNumber")) > 0:
-            issued_receipt_number = self.__get_text(result.getElementsByTagName("IssuedReceiptNumber")[0].childNodes)
-
-        response_object = {
-            "ErrorMessageCode": error_message_code,
-            "ErrorMessage": error_message,
-            "TransactionReference": transaction_reference,
-            "MNOTransactionReferenceId": mnotransaction_reference_id,
-            "Amount": amount,
-            "AmountFormatted": amount_formatted,
-            "CurrencyCode": currency_code,
-            "TransactionInitialDate": transaction_init_date,
-            "TransactionCompletionDate": transaction_comp_date,
-            "IssuedReceiptNumber": issued_receipt_number
-        }
-
-        return response_object
+        return xmltodict.parse(response)["AutoCreate"]["Response"]
 
     def ac_internal_transfer(self, currency_code, amount, beneficiary_account, beneficiary_email, narrative):
         """
@@ -365,53 +262,8 @@ class YoPay:
         xml += "</AutoCreate>"
 
         response = self.__get_xml_response(xml)
-        result = parseString(response)
 
-        status = self.__get_text(result.getElementsByTagName("Status")[0].childNodes)
-        status_code = self.__get_text(result.getElementsByTagName("StatusCode")[0].childNodes)
-
-        status_message = None
-        if len(result.getElementsByTagName("StatusMessage")) > 0:
-            status_message = self.__get_text(result.getElementsByTagName("StatusMessage")[0].childNodes)
-
-        transaction_status = None
-        if len(result.getElementsByTagName("TransactionStatus")) > 0:
-            transaction_status = self.__get_text(result.getElementsByTagName("TransactionStatus")[0].childNodes)
-
-        error_message_code = None
-        if len(result.getElementsByTagName("ErrorMessageCode")) > 0:
-            error_message_code = self.__get_text(result.getElementsByTagName("ErrorMessageCode")[0].childNodes)
-
-        error_message = None
-        if len(result.getElementsByTagName("ErrorMessage")) > 0:
-            error_message = self.__get_text(result.getElementsByTagName("ErrorMessage")[0].childNodes)
-
-        transaction_reference = None
-        if len(result.getElementsByTagName("TransactionReference")) > 0:
-            transaction_reference = self.__get_text(result.getElementsByTagName("TransactionReference")[0].childNodes)
-
-        mnotransaction_reference_id = None
-        if len(result.getElementsByTagName("MNOTransactionReferenceId")) > 0:
-            mnotransaction_reference_id = self.__get_text(
-                result.getElementsByTagName("MNOTransactionReferenceId")[0].childNodes)
-
-        issued_receipt_number = None
-        if len(result.getElementsByTagName("IssuedReceiptNumber")) > 0:
-            issued_receipt_number = self.__get_text(result.getElementsByTagName("IssuedReceiptNumber")[0].childNodes)
-
-        response_object = {
-            "Status": status,
-            "StatusCode": status_code,
-            "StatusMessage": status_message,
-            "ErrorMessage": error_message,
-            "ErrorMessageCode": error_message_code,
-            "TransactionReference": transaction_reference,
-            "TransactionStatus": transaction_status,
-            "MNOTransactionReferenceId": mnotransaction_reference_id,
-            "IssuedReceiptNumber": issued_receipt_number
-        }
-
-        return response_object
+        return xmltodict.parse(response)["AutoCreate"]["Response"]
 
     def ac_withdraw_funds(self, msisdn, amount, narrative):
         """
@@ -452,53 +304,8 @@ class YoPay:
         xml += "</AutoCreate>"
 
         response = self.__get_xml_response(xml)
-        result = parseString(response)
 
-        status = self.__get_text(result.getElementsByTagName("Status")[0].childNodes)
-        status_code = self.__get_text(result.getElementsByTagName("StatusCode")[0].childNodes)
-
-        status_message = None
-        if len(result.getElementsByTagName("StatusMessage")) > 0:
-            status_message = self.__get_text(result.getElementsByTagName("StatusMessage")[0].childNodes)
-
-        transaction_status = None
-        if len(result.getElementsByTagName("TransactionStatus")) > 0:
-            transaction_status = self.__get_text(result.getElementsByTagName("TransactionStatus")[0].childNodes)
-
-        error_message_code = None
-        if len(result.getElementsByTagName("ErrorMessageCode")) > 0:
-            error_message_code = self.__get_text(result.getElementsByTagName("ErrorMessageCode")[0].childNodes)
-
-        error_message = None
-        if len(result.getElementsByTagName("ErrorMessage")) > 0:
-            error_message = self.__get_text(result.getElementsByTagName("ErrorMessage")[0].childNodes)
-
-        transaction_reference = None
-        if len(result.getElementsByTagName("TransactionReference")) > 0:
-            transaction_reference = self.__get_text(result.getElementsByTagName("TransactionReference")[0].childNodes)
-
-        mnotransaction_reference_id = None
-        if len(result.getElementsByTagName("MNOTransactionReferenceId")) > 0:
-            mnotransaction_reference_id = self.__get_text(
-                result.getElementsByTagName("MNOTransactionReferenceId")[0].childNodes)
-
-        issued_receipt_number = None
-        if len(result.getElementsByTagName("IssuedReceiptNumber")) > 0:
-            issued_receipt_number = self.__get_text(result.getElementsByTagName("IssuedReceiptNumber")[0].childNodes)
-
-        response_object = {
-            "Status": status,
-            "StatusCode": status_code,
-            "StatusMessage": status_message,
-            "ErrorMessage": error_message,
-            "ErrorMessageCode": error_message_code,
-            "TransactionReference": transaction_reference,
-            "TransactionStatus": transaction_status,
-            "MNOTransactionReferenceId": mnotransaction_reference_id,
-            "IssuedReceiptNumber": issued_receipt_number
-        }
-
-        return response_object
+        return xmltodict.parse(response)["AutoCreate"]["Response"]
 
     def ac_acct_balance(self):
         """
@@ -515,38 +322,8 @@ class YoPay:
         xml += "</AutoCreate>"
 
         response = self.__get_xml_response(xml)
-        result = parseString(response)
 
-        status = self.__get_text(result.getElementsByTagName("Status")[0].childNodes)
-        status_code = self.__get_text(result.getElementsByTagName("StatusCode")[0].childNodes)
-
-        balances_response = {}
-        for i in range(0, len(result.getElementsByTagName("Currency"))):
-            balances_response[self.__get_text(result.getElementsByTagName("Code")[i].childNodes)] = self.__get_text(
-                result.getElementsByTagName("Balance")[i + 1].childNodes)
-
-        status_message = None
-        if len(result.getElementsByTagName("StatusMessage")) > 0:
-            status_message = self.__get_text(result.getElementsByTagName("StatusMessage")[0].childNodes)
-
-        error_message = None
-        if len(result.getElementsByTagName("ErrorMessage")) > 0:
-            error_message = self.__get_text(result.getElementsByTagName("ErrorMessage")[0].childNodes)
-
-        error_message_code = None
-        if len(result.getElementsByTagName("ErrorMessageCode")) > 0:
-            error_message_code = self.__get_text(result.getElementsByTagName("ErrorMessageCode")[0].childNodes)
-
-        response_object = {
-            "Status": status,
-            "StatusCode": status_code,
-            "StatusMessage": status_message,
-            "ErrorMessage": error_message,
-            "ErrorMessageCode": error_message_code,
-            "balances": balances_response
-        }
-
-        return response_object
+        return xmltodict.parse(response)["AutoCreate"]["Response"]
 
     def __get_xml_response(self, xml):
         """
